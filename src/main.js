24fcc4b51d51f261b22d0e9f0522c2229a87d4bd
alejox <alejoxdrums@gmail.com>
2023-02-05 18:35:46 -0500

    vue bootstrap config

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { loadFonts } from "./plugins/webfontloader";
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
 
 loadFonts();
 
-createApp(App).use(router).use(store).mount("#app");
+createApp(App).use(router).use(BootstrapVue3).use(store).mount("#app");
