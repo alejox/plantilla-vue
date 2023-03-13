ef779eafe82ff6aa3ac6730dfacf5eacae6e2c4a
alejox <alejoxdrums@gmail.com>
2023-03-13 01:58:51 -0500

    update template

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import { loadFonts } from "./plugins/webfontloader";
-import BootstrapVue3 from 'bootstrap-vue-3'
-import 'bootstrap/dist/css/bootstrap.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { loadFonts } from './plugins/webfontloader';
+import BootstrapVue3 from 'bootstrap-vue-3';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
 loadFonts();
 
-createApp(App).use(router).use(BootstrapVue3).use(store).mount("#app");
+createApp(App).use(router).use(store).use(BootstrapVue3).mount('#app');
