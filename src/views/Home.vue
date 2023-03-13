0a5f72ee7eba58bfa564e2a4a8497e9eb4a7da63
alejox <alejoxdrums@gmail.com>
2023-03-12 23:05:33 -0500

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>
-    Hola
+  <b-card>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
+  </b-card>
+    <div class="box bg-success">
+
+
   </div>
+  <p class="text-success">Hola</p>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
+
+.box{
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
 
 </style>
