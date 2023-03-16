79a2679057646bea33e59e8136c277f7a78bc32e
alejox <alejoxdrums@gmail.com>
2023-03-16 16:27:19 -0500

    updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,9 @@
 <template>
   <h1 class="title">Hello world</h1>
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, libero
-    harum aspernatur perferendis consectetur sed illum ratione magnam
-    perspiciatis sunt eius magni. Harum sapiente quisquam illo voluptate quod
-    aut doloremque.
-  </p>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped></style>
 
-</style>
-
-<script setup>
-const data = [
-  { title: 'hola', title2: 'hola' },
-  { title: 'hola', title2: 'hola' },
-];
-</script>
+<script setup></script>
