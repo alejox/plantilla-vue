55ed0ecebeb82673d76bcd34366acfbd2f649f36
alejox <alejoxdrums@gmail.com>
2023-05-18 11:09:30 -0500

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue';
 import router from './router';
 import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import './assets/sass/_typography.scss';
 import { i18n } from './plugins/i18n.js';
 import globalComponents from './plugins/global';
